89f1f77192c4704ceca9054c60d180aeb4192c55
Bastien Dejean <nihilhill@gmail.com>
2014-02-26 11:19:13 +0100

    First commit

---
+++ deheader.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Deheader < Formula
+  homepage 'http://www.catb.org/~esr/deheader'
+  url 'http://www.catb.org/~esr/deheader/deheader-0.8.tar.gz'
+  sha1 '4527b4675a7b06d728cfa989a3b7844cdf091b40'
+  head 'https://git.gitorious.org/deheader/deheader.git'
+
+  depends_on :python
+
+  def install
+    man1.install 'deheader.1'
+    bin.install 'deheader'
+  end
+
+  test do
+    system "#{bin}/deheader", '-V'
+  end
+end
---
+++ z-go.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class ZGo < Formula
+  homepage 'https://github.com/baskerville/Z'
+  url 'https://github.com/baskerville/Z/archive/0.5.1.tar.gz'
+  sha1 '7b6e7625768841993779f0c9808d7bcedd059381'
+  head 'https://github.com/baskerville/Z.git'
+
+  depends_on 'go' => :build
+
+  def install
+    system 'make'
+    system 'make', "DESTDIR=#{prefix}", 'install'
+  end
+
+  test do
+    system "#{bin}/Z", '-h'
+  end
+end
