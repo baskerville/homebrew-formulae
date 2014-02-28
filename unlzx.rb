8969212743d468ff02a5795d3ee512066b5a11f5
Bastien Dejean <nihilhill@gmail.com>
2014-02-28 14:03:44 +0100

    Add version

--- unlzx.rb
+++ unlzx.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Unlzx < Formula
   homepage 'http://xavprods.free.fr/lzx/'
   url 'http://aminet.net/misc/unix/unlzx.c.gz'
+  version '1.0'
   sha1 '752dd439a30be79da6780683137554db6d84a8a9'
 
   def install
