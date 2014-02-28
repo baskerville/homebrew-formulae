25f0343806715d81fd2d443aaa1a1be18b3d828d
Bastien Dejean <nihilhill@gmail.com>
2014-02-28 13:59:58 +0100

    Add unlzx

---
+++ unlzx.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Unlzx < Formula
+  homepage 'http://xavprods.free.fr/lzx/'
+  url 'http://aminet.net/misc/unix/unlzx.c.gz'
+  sha1 '752dd439a30be79da6780683137554db6d84a8a9'
+
+  def install
+    system "#{ENV.cc} #{ENV.cflags} -o unlzx unlzx.c"
+    bin.install('unlzx')
+  end
+end
