1d78659759c005670365308c0ecd973921527424
Bastien Dejean <nihilhill@gmail.com>
2014-03-02 19:19:01 +0100

    Add test

--- deheader.rb
+++ deheader.rb
@@ -9,7 +9,19 @@ class Deheader < Formula
   depends_on :python
 
   def install
-    man1.install 'deheader.1'
-    bin.install 'deheader'
+    bin.install "deheader"
+    man1.install "deheader.1"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <stdio.h>
+      #include <string.h>
+      int main(void) {
+        printf("%s", "foo");
+        return 0;
+      }
+    EOS
+    assert_equal "121", `deheader test.c | tr -cd 0-9`
   end
 end
