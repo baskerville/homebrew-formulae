a74731bd040dbdcac9b3bb9cab08c8a7fb353455
Bastien Dejean <nihilhill@gmail.com>
2014-07-01 12:34:00 +0200

    bspwm 0.8.9

--- bspwm.rb
+++ bspwm.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Bspwm < Formula
   homepage "https://github.com/baskerville/bspwm"
-  url "https://github.com/baskerville/bspwm/archive/0.8.8.tar.gz"
-  sha1 "df24471169c3b705754fe0dbeeec88165d218038"
+  url "https://github.com/baskerville/bspwm/archive/0.8.9.tar.gz"
+  sha1 "013a13855fdc19e555dc8e9830938db92dd5c41f"
   head "https://github.com/baskerville/bspwm.git"
 
   depends_on :x11
