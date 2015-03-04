2c08f4f9587de3e697adb0132f25319ffce4c601
Bastien Dejean <nihilhill@gmail.com>
2015-03-04 09:46:17 +0100

    stest: Add manual

--- stest.rb
+++ stest.rb
@@ -1,13 +1,14 @@
 require "formula"
 
 class Stest < Formula
-  homepage "http://tools.suckless.org/dmenu/"
-  url "http://git.suckless.org/dmenu/plain/stest.c"
-  version "0.1"
-  sha1 "2aae4710a28ee151f050b6fcaeb5eb8fad3063bf"
+  homepage "http://tools.suckless.org/dmenu"
+  url "http://git.suckless.org/dmenu/snapshot/dmenu-4.5.tar.bz"
+  head "http://git.suckless.org/dmenu", :using => :git
+  sha1 "a9f4bd4094cd1e78a147286e7372f3b236e76ae0"
 
   def install
     system "#{ENV.cc} #{ENV.cflags} -o stest stest.c"
     bin.install "stest"
+    man1.install "stest.1"
   end
 end
