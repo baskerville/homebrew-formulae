8c9af2a3ac60e0af9f282b9fba77604c22c81d4a
Bastien Dejean <nihilhill@gmail.com>
2015-08-17 16:22:34 +0200

    Add translate-shell

---
+++ translate-shell.rb
@@ -0,0 +1,17 @@
+require "formula"
+
+class TranslateShell < Formula
+  homepage "http://www.soimort.org/translate-shell"
+  url "http://www.soimort.org/translate-shell/translate-shell.tar.gz"
+  sha1 "32e35704c5d6f1f6ce2bc130967a574ac220d767"
+  version "0.9.0.6"
+
+  depends_on "curl" => :optional
+  depends_on "fribidi" => :optional
+  depends_on "gawk"
+
+  def install
+    bin.install "trans"
+    man1.install "trans.1"
+  end
+end
