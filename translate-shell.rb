4284c763cdecdd6dd055daf5b2132a490b2bafdf
Bastien Dejean <nihilhill@gmail.com>
2016-01-28 11:27:14 +0100

    translate-shell 0.9.3

--- translate-shell.rb
+++ translate-shell.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class TranslateShell < Formula
   homepage "http://www.soimort.org/translate-shell"
-  url "https://github.com/soimort/translate-shell/archive/v0.9.0.7.tar.gz"
-  sha1 "58aecf87aa134e3f8366d38ebc16d166ff186cef"
+  url "https://github.com/soimort/translate-shell/archive/v0.9.3.tar.gz"
+  sha1 "e6e415ba56a7116d2c2743c21b93257cc0c1a819"
   head "https://github.com/soimort/translate-shell.git"
 
   depends_on "gawk"
