4c72030560475701b8a6621f54337ba78cf6ec63
Fielding Johnston <fielding@justfielding.com>
2017-09-17 23:51:58 -0500

    fdb 0.3.2

--- fdb.rb
+++ fdb.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Fdb < Formula
   homepage "https://github.com/baskerville/fdb"
-  url "https://github.com/baskerville/fdb/archive/0.1.0.tar.gz"
-  sha256 "04c210d2a38d15e98d3f3d881a3999448f77f221454691c9131a6ea3f111fb99"
+  url "https://github.com/baskerville/fdb/archive/0.3.2.tar.gz"
+  sha256 "462454e0254556b599d86976ee750664aae0369253d2764f8d44f4ed97cba4d4"
   head "https://github.com/baskerville/fdb.git"
 
   depends_on "rust" => :build
