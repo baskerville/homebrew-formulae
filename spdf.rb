9fe8498256c5aa65266f77efcd4ede08c70f85e1
dr2m <maletin@maletin.work>
2018-05-07 13:20:35 +0300

    spdf: Fix outdated python resource specification

--- spdf.rb
+++ spdf.rb
@@ -5,7 +5,11 @@ class Spdf < Formula
   head "https://github.com/baskerville/spdf.git"
 
   depends_on "python@2"
-  depends_on "PyPDF2" => :python
+
+  resource "PyPDF2" do
+    url "https://files.pythonhosted.org/packages/b4/01/68fcc0d43daf4c6bdbc6b33cc3f77bda531c86b174cac56ef0ffdb96faab/PyPDF2-1.26.0.tar.gz"
+    sha256 "e28f902f2f0a1603ea95ebe21dff311ef09be3d0f0ef29a3e44a932729564385"
+  end
 
   def install
     bin.install "spdf.py" => "spdf"
