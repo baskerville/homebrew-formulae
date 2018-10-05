cb61a4f817a2adab4cf6ab01c11dc8482781b3fa
Fielding Johnston <fielding@justfielding.com>
2018-10-05 10:16:29 -0500

    sxhkd 0.5.9

--- sxhkd.rb
+++ sxhkd.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Sxhkd < Formula
   homepage "https://github.com/baskerville/sxhkd"
-  url "https://github.com/baskerville/sxhkd/archive/0.5.8.tar.gz"
-  version "0.5.8"
-  sha256 "d7df48d7d301d1cf06f45172bcfe712510e98dcdbdb896af662cf979f9666855"
+  url "https://github.com/baskerville/sxhkd/archive/0.5.9.tar.gz"
+  version "0.5.9"
+  sha256 "457e0fdfe0a7dac24ca506c026a1e8cde5899073ba1d2ef1326bc68b931648bd"
   head "https://github.com/baskerville/sxhkd.git"
 
   depends_on :x11
