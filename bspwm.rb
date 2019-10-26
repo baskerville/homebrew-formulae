2559c96250ca3974690cd2c8ef8979178b4e0b1c
Fielding Johnston <fielding@justfielding.com>
2019-10-26 11:37:55 -0500

    bspwm 0.9.9

--- bspwm.rb
+++ bspwm.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Bspwm < Formula
   homepage "https://github.com/baskerville/bspwm"
-  url "https://github.com/baskerville/bspwm/archive/0.8.9.tar.gz"
-  sha256 "750c76132914661d8d5edf7809e9b601977215d31e747dd780c60fd562913d55"
+  url "https://github.com/baskerville/bspwm/archive/0.9.9.tar.gz"
+  sha256 "b1438c3cad13b6101edb31ce8d00ba4ed2f972754e85b90f763e04fa5143c6fc"
   head "https://github.com/baskerville/bspwm.git"
 
   depends_on :x11
